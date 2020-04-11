552175a24567f4d19d3d89fcba825f24986f0400
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2020-04-10 21:44:07 -0300

    modify Photogrid component

--- src/components/PhotoGrid.js
+++ src/components/PhotoGrid.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-
+import posts from "../postExamples";
 
 function PhotoGrid() {
   return (
     <div>
       <h3>This is the grid</h3>
       <ul>
-        <li> <Link to="/grid/:photoId">Example</Link></li> 
-        <li> <Link to="/grid/:photoId">Example</Link></li> 
-        <li> <Link to="/grid/:photoId">Example</Link></li> 
-        <li> <Link to="/grid/:photoId">Example</Link></li> 
+        {/* {posts.map(item => (
+          // <li> <Link to="/grid/:photoId"><img width="50px" src={`${item.display_src}`}/></Link></li>
+        ))} */}
+        <li> <Link to="/grid/:photoId">pic</Link></li>
       </ul>
     </div>
   );
