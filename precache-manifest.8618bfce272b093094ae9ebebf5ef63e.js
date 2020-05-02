bbcfe44744cc0ae712aeaf9a3b57d84e84d25efd
Martin Wiytiszen <martinwiytiszen@hotmail.com>
2020-05-02 02:07:31 -0300

    Updates

---
+++ precache-manifest.8618bfce272b093094ae9ebebf5ef63e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d10284d22b31491f2094fb98d5fcc23",
+    "url": "/fake-stagram/index.html"
+  },
+  {
+    "revision": "77ef519a6f185a86b6cf",
+    "url": "/fake-stagram/static/css/main.0ca3a67e.chunk.css"
+  },
+  {
+    "revision": "a146c37cdc6263b9e6e7",
+    "url": "/fake-stagram/static/js/2.7715161c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fake-stagram/static/js/2.7715161c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ef519a6f185a86b6cf",
+    "url": "/fake-stagram/static/js/main.c2b390a8.chunk.js"
+  },
+  {
+    "revision": "9bbcb0665e715c5ba961",
+    "url": "/fake-stagram/static/js/runtime-main.aa500259.js"
+  }
+]);
